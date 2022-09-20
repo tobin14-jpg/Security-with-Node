c0268b2abc16b2003f3e0c395347efd983615c53
Ice Tea <shauntobin88@gmail.com>
2022-09-20 20:30:16 +0800

    registered application on OAUTH + log in functionality

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 const app = express();
 
-// get secret data
-app.get('/secret', (req, res) => {
+// add security at the top of the middleware
+app.use(helmet());
+
+// Check if the user is logged in
+function checkLoggedin(req, res, next)  {
+    const isLoggedIn = true;
+    if(!isLoggedIn) {
+        return res.status(401).json({
+
+        })
+    }
+    next();
+}
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+// get secret data + protect endpoint by restricting access to only logged in users using Google's OAUTH
+app.get('/secret', checkLoggedin, (req, res) => {
     return res.send('Your secret value is 14!');
-})
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
